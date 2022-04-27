e2dc84dc4c9b751a77f1c7da1c800dc3c0012eb2
Joliph Christian <joliph.c@simformsolutions.com>
2022-04-27 18:09:17 +0530

    Initial Commit

---
+++ MoyaDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoyaDemo
+//
+//  Created by Joliph Christian on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
